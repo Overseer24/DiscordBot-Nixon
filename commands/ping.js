77dffb93f45253b25e1fe3b30251b47a6599c551
Nixon <nixon.somoza@gmail.com>
2025-03-06 05:02:08 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,23 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder} = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Just Ping Me!!!'),
     async execute(interaction) {
-        if (interaction.replied || interaction.deferred) {
-            return interaction.editReply("Pong!");
-        }
-        return interaction.reply("Pong!");
+        const gifURL = "https://i.makeagif.com/media/8-02-2015/cg42o6.gif";
+
+        const embed = new EmbedBuilder()
+            .setColor('DarkRed')
+            .setTitle("PPPPPPOOOONG")
+            .setDescription(`UGHHH`)
+            .setImage(gifURL);
+
+            if (interaction.replied || interaction.deferred) {
+                return interaction.editReply({ embeds: [embed] }); // Use editReply if already replied/deferred
+            } else {
+                return interaction.reply({ embeds: [embed] }); // Use reply if not yet replied/deferred
+            }
+       
     }
 };
